eb97e4aa4f5c4200b367aea3468464fd3e2b113b
stvnlee890 <stvnlee890@gmail.com>
2022-10-11 11:01:20 -0700

    authentication and authorization tested

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const express = require('express'); 
-const app = express(); 
-cors = require('cors')
-const helmet = require('helmet');
+const express = require("express");
+const app = express();
+cors = require("cors");
+const helmet = require("helmet");
 const morgan = require("morgan");
 
-
 // MIDDLEWARE
-app.use(cors())
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: true }))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("common"));
 
@@ -16,12 +15,15 @@ app.use(morgan("common"));
 //     res.redirect('api/users');
 // });
 
-// ROUTES 
-const userController = require('./routes/UserController')
-app.use("/api/users", userController); 
-// app.use("/api/auth", authRoute); 
-// app.use("/api/posts", postRoute); 
+// ROUTES
+const userController = require("./routes/userController");
+app.use("/api/users", userController);
 
-app.listen(8800,()=>{
-    console.log("Backend Server is running! Local Host: 8800"); 
-})
+const blogPostController = require("./routes/blogPostController");
+app.use("/api/blogpost", blogPostController)
+// app.use("/api/auth", authRoute);
+// app.use("/api/posts", postRoute);
+
+app.listen(8800, () => {
+  console.log("Backend Server is running! Local Host: 8800");
+});

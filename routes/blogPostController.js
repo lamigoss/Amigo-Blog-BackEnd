f811a4486541043cff39fe6ba648e8992fbc4f99
stvnlee890 <stvnlee890@gmail.com>
2022-10-11 19:46:58 -0700

    post

--- routes/blogPostController.js
+++ routes/blogPostController.js
@@ -1,23 +1,23 @@
 const router = require("express").Router();
-const BlogPost = require('../model/blogPost')
+const BlogPost = require("../model/blogPost");
 
-router.get('/', async (req, res, next) => {
-    try {
-        const blog = await BlogPost.findById(req.body.id)
-        res.status(200).json(blog)  
-    } catch(error) {
-        next(error)
-    } 
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const blog = await BlogPost.findById(req.body.id);
+    res.status(200).json(blog);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.post('/', async (req, res, next) => {
-    try {
-        const blog = await BlogPost.create(req.body)
-        const populate = await blog.populate('owner')
-        res.status(201).json(blog)
-    } catch(error) {
-        next(error)
-    }
-})
+router.post("/", async (req, res, next) => {
+  try {
+    const blog = await BlogPost.create(req.body);
+    const populate = await blog.populate("owner");
+    res.status(201).json(blog);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;

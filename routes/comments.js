fa0933fe5a8b76a848a9a8f098944f6fb7bf6e15
stvnlee890 <stvnlee890@gmail.com>
2022-10-27 23:41:48 -0700

    had to change the post comments route abit

--- routes/comments.js
+++ routes/comments.js
@@ -1,45 +1,38 @@
 const router = require("express").Router();
 const Comment = require("../model/Comment");
 
-router.post("/",  async (req, res, next)=>{
-    const newComment = new Comment({...req.body, userId: req.body.userId})
- try {
-    const savedComment = await newComment.save();
-    const populate = await savedComment.populate("postId");
-
-    console.log("error reslove please"+savedComment);
-    res.status(200).send(savedComment);
- } catch (err) {
-     next(err);
- }
+router.post("/", async (req, res, next) => {
+  console.log(req.body);
+  try {
+    const newComment = await Comment.create(req.body);
+    const populate = await newComment.populate("postId");
+    res.status(200).send(newComment);
+  } catch (err) {
+    next(err);
+  }
 });
 
-
-//get all comments on a post 
-router.get("/:postId", async (req, res)=>{
-
- try {
-    const comments = await Comment.find({postId: req.params.postId});
+//get all comments on a post
+router.get("/:postId", async (req, res) => {
+  try {
+    const comments = await Comment.find({ postId: req.params.postId });
     res.status(200).send(comments);
- } catch (err) {
-    next(err)
-     console.log(err);
- }
+  } catch (err) {
+    next(err);
+    console.log(err);
+  }
 });
 
-
-//delete a comment 
-router.delete("/:postId/:commentId", async (req, res)=>{
-
-    try {
-       const post = await Comment.findOneAndDelete({_id: req.params.commentId});
+//delete a comment
+router.delete("/:postId/:commentId", async (req, res) => {
+  try {
+    const post = await Comment.findOneAndDelete({ _id: req.params.commentId });
     //    const comment =  post.find({_id: req.params.commentId});
-       res.status(200).send("comment deleted");
-    } catch (err) {
-       next(err)
-        console.log(err);
-    }
-   });
+    res.status(200).send("comment deleted");
+  } catch (err) {
+    next(err);
+    console.log(err);
+  }
+});
 
-
-module.exports = router; 
+module.exports = router;
